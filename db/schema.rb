d45f87edee00a6215489af7e154e0a2b36a8430c
Derek Young <derekpyoung12@gmail.com>
2022-08-09 10:27:03 -0700

    postgres db

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_25_170059) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "rounds", force: :cascade do |t|
     t.integer "bet_per_hole"
     t.integer "bet_per_kp"
