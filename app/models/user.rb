03e2ecf37cc09ee1dec1d46a0f5fbcb7b26d08c7
Derek Young <derekpyoung12@gmail.com>
2023-03-13 14:38:10 -0700

    email not case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_secure_password 
   validates :email, presence: true, uniqueness: true
   validates :name, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
 
   def downcase_name
     self.name = name.downcase
